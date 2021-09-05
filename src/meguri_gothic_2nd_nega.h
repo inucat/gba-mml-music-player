64c0b0a98ab4be08fb647bf1c3c0f63eae6905d9
inucat-ucremix <inucat@ucremix>
2021-09-05 14:40:29 +0900

    fonts

---
+++ src/meguri_gothic_2nd_nega.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(meguri_gothic_2nd_nega)
+
+//======================================================================
+//
+//	meguri_gothic_2nd_nega, 128x128@8, 
+//	+ palette 256 entries, not compressed
+//	+ 256 tiles not compressed
+//	Total size: 512 + 16384 = 16896
+//
+//	Time-stamp: 2021-09-05, 14:01:23
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_MEGURI_GOTHIC_2ND_NEGA_H
+#define GRIT_MEGURI_GOTHIC_2ND_NEGA_H
+
+#define meguri_gothic_2nd_negaTilesLen 16384
+extern const unsigned char meguri_gothic_2nd_negaTiles[16384];
+
+#define meguri_gothic_2nd_negaPalLen 512
+extern const unsigned short meguri_gothic_2nd_negaPal[256];
+
+#endif // GRIT_MEGURI_GOTHIC_2ND_NEGA_H
+
+//}}BLOCK(meguri_gothic_2nd_nega)
